d2b0d69f47b978f44adc29154c421dccf40db298
Simon Walz <contact@simonwalz.de>
2025-12-30 19:19:32 +0100

    refact: async read

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
 const owfs = require("owfs");
 
 exports.init = function(node, app_config, main) {
-	var timer = [];
+	const timer = [];
 
-	var get_temperature_interval = function(node, sid) {
-		var file = "/" + sid + "/temperature";
-		var still_active = null;
-		var t = setInterval(function() {
-			if (still_active) {
-				var diff = (new Date() - still_active)/1000;
-				if (diff < 120) {
-					console.log("owfs:", sid,
-						"interval still active since",
-						still_active);
-					return;
-				}
-			}
-			still_active = new Date();
-			connection.read(file, function(err, temp) {
-				still_active = null;
-				if (err) {
-					console.error("OWFS", sid, err);
-				}
-				//console.log(sid, "temperature", +temp);
-				if (typeof temp !== "undefined") {
-					node.publish(undefined, +temp);
-				} else {
-					node.publish(undefined, null);
-				}
-			});
-		}, (app_config.interval || 5) * 1000);
-		timer.push(t);
-	};
-
-	var connection = new owfs.Client(
+	const connection = new owfs.Client(
 		app_config.host || 'localhost',
 		app_config.port || 4304
 	);
 
+	let cancel_fn = null;
+	const p_cancel = new Promise((resolve, reject)=>{
+		cancel_fn = ()=>{
+			reject("canceled");
+		};
+	});
+
+	function read(file) {
+		const p = new Promise((resolve, reject)=>{
+			const read_tid = setTimeout(()=>{
+				reject(new Error("OWFS Timed out"));
+			}, 4000);
+			connection.read(file, (err, result) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+				clearTimeout(read_tid);
+			});
+			timer.length = 0;
+			timer.push(read_tid);
+		});
+		return Promise.race([p, p_cancel]);
+	}
+
+	async function read_interval(directory) {
+		for (const file of directory) {
+			const sid = file.replace(/^\//, "");
+			const n = map.node(sid);
+			if (!n) return;
+
+			try {
+				console.log("READ", sid);
+				let temp = await read(file + "/temperature");
+				console.log("TEMP", sid, temp);
+				if (typeof temp === "string") {
+					temp = +temp;
+				}
+				node.publish(undefined, +temp);
+			} catch(err) {
+				if (err === "canceled") return;
+				console.error("OWFS", sid, err);
+			}
+		}
+		timer.push(
+			setTimeout(read_interval.bind(null, directory),
+				(app_config.interval || 5) * 1000
+			)
+		);
+	}
+
 	var map = node.map(app_config.map, null, true, undefined,
 		function(n, metadata, c) {
-			var sid = c.map;
-
-			if (c.deactive) return;
-
-			get_temperature_interval(n, sid);
-
 			n.announce([{
 				"type": "temperature.data",
 				"unit": "C",
@@ -57,13 +73,8 @@ exports.init = function(node, app_config, main) {
 			console.error("owfs Error", err);
 			return;
 		}
-		//console.log(directory);
-		directory.forEach(function(file) {
-			let sid = file.replace(/^\//, "");
-			var n = map.node(sid);
-			if (!n) return;
-		});
+		read_interval(directory);
 	});
 
-	return [map, timer, connection];
+	return [cancel_fn, map, timer, connection];
 };
